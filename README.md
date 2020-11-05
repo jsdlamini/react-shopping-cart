338d4544f953448e2e533ab6486fa3f49a5d2318
Johnson Dlamini <johnsjdsd@gmail.com>
2020-11-05 10:21:17 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+#React Shopping Cart
+
+
+
+#React R
---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+function App() {
+  return (
+    <div className="grid-container">
+      <header>
+        <a href="/">React Shopping Cart</a>
+      </header>
+      <main>Product list</main>
+      <footer>All rights reserved</footer>
+    </div>
+  );
+}
+
+export default App;
